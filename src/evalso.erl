4351ce192eded208ad0578bcb4d43501d65bd367
mokevnin <mokevnin@gmail.com>
2014-01-21 11:42:28 +0400

    change client api

--- src/evalso.erl
+++ src/evalso.erl
@@ -1,6 +1,12 @@
+%TODO spec
 -module(evalso).
 
--export([evaluate/2]).
+-export([start/0, evaluate/2]).
+
+start() ->
+  Apps = [ibrowse, jsx],
+  [application:start(App) || App <- Apps],
+  ok.
 
 evaluate(Lng, Text) ->
   Body = [
@@ -8,14 +14,15 @@ evaluate(Lng, Text) ->
     {code, iolist_to_binary(Text)}
   ],
 
-  case ibrowse:send_req("http://eval.so/api/evaluate",
-      [{"Content-Type", "application/json"}], post, jsx:encode(Body)) of
+  {ok, ApiHost} = application:get_env(evalso, api_host),
+  EvaluateUrl = ApiHost ++ "/evaluate",
+  case ibrowse:send_req(EvaluateUrl, [{"Content-Type", "application/json"}], post, jsx:encode(Body)) of
     {ok, _, _, Json} ->
       case jsx:decode(iolist_to_binary(Json)) of
         [{<<"stdout">>, Stdout}, {<<"stderr">>, Stderr}, _, {<<"exitCode">>, 0}] ->
-          {ok, success_exit, Stdout, Stderr};
+          {ok, {success_exit, Stdout, Stderr}};
         [{<<"stdout">>, Stdout}, {<<"stderr">>, Stderr}, _, {<<"exitCode">>, 1}] ->
-          {ok, bad_exit, Stdout, Stderr}
+          {ok, {bad_exit, Stdout, Stderr}}
       end;
     {error, Reason} -> {error, Reason}
   end.
